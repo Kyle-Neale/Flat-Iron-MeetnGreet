eff677041b13667794eb2170f23a4d40192b657a
scarlettperry <si.perry15@gmail.com>
2018-08-23 08:42:46 -0400

    fixed signup bug

--- FlatIronApp/app/controllers/users_controller.rb
+++ FlatIronApp/app/controllers/users_controller.rb
@@ -24,6 +24,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
       flash[:errors] = @user.errors.full_messages
